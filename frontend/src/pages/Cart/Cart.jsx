b790bfcb9e649617352bf63a560e5d19dc95b73d
Your Name <mansoonpaudel@gmail.com>
2025-11-09 20:58:35 +0545

    halfway through the landing page

---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import'./Cart.css'
+
+const Cart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cart
